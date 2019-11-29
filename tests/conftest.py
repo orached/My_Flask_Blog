cad053f9071b3cfbc24a7720a247bd9ac59400f6
orached <orached@gmail.com>
2019-11-29 19:17:06 +0100

    modified stuff

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+
+#@pytest.fixture
+#users = [
+#    {
+#        'username': 'TesterOne',
+#        'email': 'testerone@test.com',
+#        'password': 'password',
+#    },
+#    {
+#        'username': 'TesterTwo',
+#        'email': 'testertwo@test.com',
+#        'password': 'password',
+#    }
+#]
+#posts = [
+#    {
+#        'author': 'TesterOne',
+#        'title': 'Blog Post 1',
+#        'content': 'First post content',
+#        'date_posted': 'April 20, 2019'
+#    },
+#    {
+#        'author': 'TesterTwo',
+#        'title': 'Blog Post 2',
+#        'content': 'Second post content',
+#        'date_posted': 'April 21, 2019'
+#    }
+#]
